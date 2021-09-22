9799a7d2ad1324b34a2459767b6efb3c73b90d7d
Laura Merris <laura.merris@googlemail.com>
2021-09-22 22:11:27 +0100

    testing out modal implementation for platform popup

---
+++ src/MainButton.screen.style.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+
+const styles = StyleSheet.create({
+    buttonMain: {
+        paddingHorizontal:10,
+        paddingVertical:16,
+        borderRadius:10,
+        backgroundColor:'#c0efe0',
+        color:'#393939',
+        marginBottom:24,
+        textAlign:"center",
+      },
+      buttonMainText:{
+        fontSize:24,
+        textAlign:"center"
+      },
+});
+
+export default styles;
